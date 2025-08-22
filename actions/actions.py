8251f7faed56859eee2bf920380af99eb89ab6e3
nisemo <nisemo2007@gmail.com>
2025-08-21 23:10:53 -0500

    Primer commit

---
+++ actions/actions.py
@@ -0,0 +1,51 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from datetime import datetime
+
+class ActionTellTime(Action):
+    def name(self) -> Text:
+        # El nombre debe coincidir con el definido en domain.yml
+        return "action_tell_time"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # Obtener la hora actual
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        
+         #Enviar la respuesta al usuario
+        dispatcher.utter_message(text=f"The current time is {current_time}")
+        
+        return []
